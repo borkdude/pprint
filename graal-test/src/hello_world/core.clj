a92e629529b428438896a313adf099d537a76dcb
Michiel Borkent <michielborkent@gmail.com>
2020-09-23 23:58:01 +0200

    works :)

--- graal-test/src/hello_world/core.clj
+++ graal-test/src/hello_world/core.clj
@@ -1,7 +1,8 @@
 (ns hello-world.core
   (:gen-class)
   #_(:require [clojure.pprint :refer [pprint]]) ;; => 29015008 binary size on macOS
-  (:require [babashka.pprint :refer [pprint cl-format write]]))
+  (:require [babashka.pprint :refer [pprint cl-format write]]) ;=> 10110560 binary size on macOS
+  )
 
 (defn -main [& _args]
   (pprint (zipmap (map (comp keyword str char) (range 97 117)) (range 20)))
